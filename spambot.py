17730d672ef51dd911e58c06afae3e1b0c0c2ff2
W-Jermome <102903734+W-Jermome@users.noreply.github.com>
2022-04-03 13:02:11 +0200

    Add files via upload

---
+++ spambot.py
@@ -0,0 +1,24 @@
+#if you have any problems dm Lui#9833
+
+import time
+import keyboard
+
+count = 0
+
+print("Easy python Spambot by Lui#9833..")
+
+print("1..")   #time.sleep(1) is a second it waits
+time.sleep(1) 
+print("2..")
+time.sleep(1)
+print("3..")
+time.sleep(1)
+print("4..")
+time.sleep(1)
+print("5..")
+time.sleep(1)
+
+while (count < 3):   
+    keyboard.write("Du wurdest ÜberfahrenNN!!!")
+    keyboard.press_and_release('enter')
+    time.sleep(0.3) #the speed that it spams (If you remove this your Pc will be very slow)
---
+++ spambotV2.py
@@ -0,0 +1,39 @@
+#if you have any problems dm Lui#9833
+
+import keyboard
+import time 
+import sys
+
+count = 0
+
+pen = ("Penis")
+one = ("1")
+nona = ("NN")
+zk = ("Im zukrass!!!")
+
+print("Spambot by Lui#9833!!!")
+
+print("1. Penis")
+time.sleep(0.5)
+print("2. 1")
+time.sleep(0.5)
+print("3. NN")
+time.sleep(0.5)
+print("4. Im zukrass")
+time.sleep(0.5)
+
+choice = input("Choose your spam(1/2/3/4)")
+
+while (count < 1):
+    time.sleep(0,5)
+    if choice in ('1', '2', '3', '4'):
+        if choice == "1":
+            keyboard.write(pen)
+        elif choice == "2":
+            keyboard.write(one)
+        elif choice == "3":
+            keyboard.write(nona)
+        elif choice == "4":
+            keyboard.write(zk)
+
+            
